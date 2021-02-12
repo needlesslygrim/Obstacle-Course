17c5a93377d3f5fba8a2afc7f7d1a7c75fbfeead
Erick Howard <goldkillerracer2@gmail.com>
2021-02-12 17:58:16 +0800

    Made variables
    Refactored to have variables instead of pre-set values for the frame by frame translate

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
 
+    float xValue = 0f;
+    float yValue = 0.01f;
+    float zValue = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(1,0, 0);
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f,0, 0);
+        transform.Translate(xValue, yValue, zValue);
     }
 }
