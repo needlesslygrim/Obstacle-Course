cf57b7b9ff17488b4bb63deda0cce2e906975d42
Erick Howard <goldkillerracer2@gmail.com>
2021-02-13 09:19:02 +0800

    Instructions and refactoring
    Extracted the ProcessMovement method and made a new method that prints instructions to the console

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -6,15 +6,28 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ProcessMovement();
+    }
+
+    private void ProcessMovement()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         float yValue = Input.GetAxis("UpAndDown") * Time.deltaTime * moveSpeed;
+        
         transform.Translate(xValue, yValue, zValue);
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Use WASD or the Arrow Keys to move around");
+        Debug.Log("Avoid the walls");
     }
 }
