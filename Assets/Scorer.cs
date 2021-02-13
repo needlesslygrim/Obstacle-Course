6dffd84312af313a1087bc22d3ce6a5abefc1215
Erick Howard <goldkillerracer2@gmail.com>
2021-02-13 09:53:38 +0800

    Scoring and an Obstacle
    Stopped the ObjectHit script printing that a wall has been hit, made a scoreboard script that updates the score every time a collision happens, made an obstacle

--- Assets/Scorer.cs
+++ Assets/Scorer.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectHit : MonoBehaviour
+public class Scorer : MonoBehaviour
 {
+    private int hits = 0;
     private void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.yellow;
+        hits++;
+        Debug.Log("You've bumped into a thing this many times: " + hits);
     }
 }
